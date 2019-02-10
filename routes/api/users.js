ad76b2f1bd2fbbd9a66b66784b690c549efe799d
David Velarde <david.velarde.robles@mobgen.com>
2019-02-10 08:53:43 +0100

    Added model schema and register api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const gravatar = require("gravatar");
+
+//Load User model
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests user route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res
+        .status(400)
+        .json({ key: "email", value: "Email already exists" });
+    } else {
+      let avatar = gravatar.url(req.body.email, {
+        s: "200", //Size
+        r: "pg", //Rating
+        d: "mm" //Default
+      });
+      let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          } else {
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                res.json(user);
+              })
+              .catch(error => {
+                console.log(error);
+              });
+          }
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
